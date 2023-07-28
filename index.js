a3a987c64ea8d70537ff3bdbea2f9f2fe6765347
h0ss3in88 <husseinDeveloperx@protonmail.com>
2023-07-29 03:51:45 +0430

    add connecting to mongodb atlas cloud functionality and refactoring starting express application in entry file successfully

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 const http = require("http");
-const {app} = require("./src/server/app");
-const server = http.createServer(app);
-server.listen(app.get("PORT"), () => {
-    console.log(`blog running at ${server.address().address}:${server.address().port}`);
-});
+const {createApp} = require("./src/server/app");
+const {Db} = require('./src/server/db');
+const dotenv = require("dotenv");
+(async function connectToDb() {
+    try {
+        console.log("----------------------------------------------");
+        console.log("---------CONNECTING TO DATABASE --------------");
+        dotenv.config();
+        console.log(process.env.MONGODB_CONNECTION_STRING);
+        const connectionString = process.env.MONGODB_CONNECTION_STRING;
+        let dataAccess = new Db({connectionString});
+        await dataAccess.connect();
+        let app = await createApp({dataAccess});
+        const server = http.createServer(app);
+        server.listen(app.get("PORT"), () => {
+            console.log(`blog running at ${server.address().address}:${server.address().port}`);
+        });
+    } catch (error) {
+        console.log(error);
+        console.log(`ERROR occurred during initialization ${error}`);
+    }
+})();
